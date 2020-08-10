c1affff2694b12b98113086e64bfabbce3486f84
HilliamT <hilliamtung@gmail.com>
2020-08-10 16:01:49 +0100

    Abstracted away current centre of the map

--- components/Map.js
+++ components/Map.js
@@ -2,8 +2,8 @@ import { Map, TileLayer, Marker, Popup } from "react-leaflet-universal";
 import L from 'leaflet'
 
 export default class MapElem extends React.Component {
-
     state = {
+        currentLocation: [51.505, -0.09],
         restaurants: [{ latitude: 51.505, longitude: -0.09 }]
     }
 
@@ -12,7 +12,7 @@ export default class MapElem extends React.Component {
     }
 
     render() {
-        return (<Map center={[51.505, -0.09]} zoom={13} style={{ height: "100vh" }}>
+        return (<Map center={this.state.currentLocation} zoom={13} style={{ height: "100vh" }}>
             <TileLayer
                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
