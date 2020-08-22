2c6aeac8a7c53254fcf75bc6b8394af8ffcd3549
HilliamT <hilliamtung@gmail.com>
2020-08-22 17:55:05 +0100

    Using a long-lat, postcode of location is obtained

--- pages/api/getRestaurants.js
+++ pages/api/getRestaurants.js
@@ -1,11 +1,16 @@
 import csv from "csvtojson";
 import axios from "axios";
 import cache from "memory-cache";
+import postcodes from "node-postcodes.io"
 
 const postcode_to_longlat = require('../../preprocessing/postcode_to_longlat.json');
 
-
 export default async (req, res) => {
+    let { result } = await postcodes.geo(51.50350519362193, -0.06000660302581952);
+    if (result.length == 0) return res.status(400).send({ error: "Not enough results" });
+    console.log(result[0].postcode);
+
+    /*
     const value = cache.get("restaurants");
 
     if (value) {
@@ -30,6 +35,10 @@ export default async (req, res) => {
         //restaurants.push({ ...restaurant, latitude, longitude });
         return { ...restaurant, latitude, longitude };
     });
+    
 
     res.status(200).send(restaurants);
+    */
+
+    res.status(200).send({ results });
 }
